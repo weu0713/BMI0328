9b96ea23d53bbd6850812d827ff4b14072faff6f
weu0713 <h24463179@mailst.cjcu.edu.tw>
2018-04-11 15:59:03 +0800

    第四次練習(跳出視窗)

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cjcu.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("確定",null)
                 .setNegativeButton("cancel",null)
                 .show();
+        Intent intent = new Intent(this,ResultActivity.class);
 
+        startActivity(intent);
     }
 }
